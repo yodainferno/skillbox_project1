c74813a49f620309f73cb5cc2894ce19a2e00099
yodainferno <p@namoy.ru>
2023-02-20 18:53:16 +0300

    4.3 task

--- src/App.vue
+++ src/App.vue
@@ -3,60 +3,7 @@
     <ul class="catalog__list">
 
       <ProductList :products="products" :title="title"/>
-    </ul>
-
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a
-          class="pagination__link pagination__link--arrow pagination__link--disabled"
-          aria-label="Предыдущая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current">
-          1
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          2
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          3
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          4
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          ...
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          10
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a
-          class="pagination__link pagination__link--arrow"
-          href="#"
-          aria-label="Следующая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
+      <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
     </ul>
   </section>
 </template>
@@ -64,15 +11,26 @@
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
+import BasePagination from './components/BasePagination.vue';
 
 export default {
   name: 'App',
-  components: { ProductList },
+  components: { ProductList, BasePagination },
   data() {
     return {
-      products: products,
-      title: 'Каталог моих товаров'
+      page: 1,
+      productsPerPage: 3,
+      title: 'Каталог моих товаров',
     };
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 
