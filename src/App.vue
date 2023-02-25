ee84cc66ef7842ea02738666b38eb4ba47d74b34
yodainferno <p@namoy.ru>
2023-02-25 23:00:45 +0300

    4.5

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,73 @@
 <template>
-  <section class="catalog">
-    <ul class="catalog__list">
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
 
-      <ProductList :products="products" :title="title"/>
-      <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
-    </ul>
-  </section>
+    <div class="content__catalog">
+      <ProductFilter
+        :price-from.sync="filterPriceFrom"
+        :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId"
+      />
+
+      <section class="catalog">
+        <ul class="catalog__list">
+          <ProductList :products="products" :title="title"/>
+          <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
+        </ul>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+       
       page: 1,
       productsPerPage: 3,
       title: 'Каталог моих товаров',
     };
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = products
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter(product => product.price > this.filterPriceFrom);
+      }
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter(product => product.price < this.filterPriceTo);
+      }
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts.filter(product => product.categoryId === this.filterCategoryId);
+      }
+
+      return filteredProducts
+    },
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products.length;
+      return this.filteredProducts.length;
     },
   },
 };
