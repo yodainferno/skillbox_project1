ab7fb9c8207910e5429cc1cfa52544fa1fe9df74
yodainferno <p@namoy.ru>
2023-03-30 16:24:19 +0300

    7.2

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -31,7 +31,8 @@
   import ProductList from '@/components/ProductList.vue';
   import BasePagination from '@/components/BasePagination.vue';
   import ProductFilter from '@/components/ProductFilter.vue';
-  
+  import axios from 'axios';
+
   export default {
     components: { ProductList, BasePagination, ProductFilter },
     data() {
@@ -43,6 +44,8 @@
         page: 1,
         productsPerPage: 3,
         title: 'Каталог моих товаров',
+
+        productsData: null
       };
     },
     computed: {
@@ -62,13 +65,36 @@
         return filteredProducts
       },
       products() {
-        const offset = (this.page - 1) * this.productsPerPage;
-        return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+        // const offset = (this.page - 1) * this.productsPerPage;
+        // return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+        return this.productsData
+          ? this.productsData.items.map(product => {
+            return {
+              ...product,
+              image: product.image.file.url
+            }
+          })
+          : []
       },
       countProducts() {
-        return this.filteredProducts.length;
+        return this.productsData ? this.productsData.pagination.total : 0
+        // return this.filteredProducts.length;
       },
     },
+    methods: {
+      loadProducts() {
+        axios.get(`https://vue-study.skillbox.cc/api/products?page=${this.page}&limit=${this.productsPerPage}`)
+          .then(reponse => this.productsData = reponse.data)
+      }
+    },
+    created() {
+      this.loadProducts()
+    },
+    watch: {
+      page() {
+        this.loadProducts()
+      }
+    }
   };
   
   </script>
